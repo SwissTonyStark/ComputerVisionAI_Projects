080217273fb1f30aa3d7fd661ba4aa9a00c9f2d8
SwissTonyStark <pol.fernandez@noser.com>
2024-11-25 12:57:45 +0100

    New env variables

--- 2.ReceiptInterpreter/scripts/notion_uploader.py
+++ 2.ReceiptInterpreter/scripts/notion_uploader.py
@@ -5,14 +5,14 @@ import yaml
 with open("config.yaml", "r") as file:
     config = yaml.safe_load(file)
 
-notion = Client(auth=config["notion_api_key"])
+notion = Client(auth=config["NOTION_API_KEY"])
 
 def send_to_notion(recipe_data):
     """
     Uploads recipe information to a Notion database.
     """
     notion.pages.create(
-        parent={"database_id": config["database_id"]},
+        parent={"DATABASE_ID": config["DATABASE_ID"]},
         properties={
             "Name": {"title": [{"text": {"content": recipe_data['name']}}]},
             "Ingredients": {"rich_text": [{"text": {"content": recipe_data['ingredients']}}]},
